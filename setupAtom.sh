346e0f55f22b1fe8c560817e495774d7c8d78228
Todd Benson <todd.benson@csaa.com>
2016-12-27 10:22:53 -0700

    Create setupAtom.sh

---
+++ setupAtom.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+apm install symbols-tree-view
+apm install merge-conflicts
+apm install pigments
+apm install color-picker
+apm install linter
+sudo pip install pylint
+pip install flake8
+pip install flake8-docstrings
+apm install linter-flake8
+apm install script
+apm install concourse-vis
+apm install open-recent
+apm install todo-show
+apm install highlight-selected
+apm install atom-beautify
+apm install auto-detect-indentation
+apm install file-icons
+apm install git-plus
+apm install emmet
+apm install zen
+apm install dash
+apm install date
+apm install linter-write-good
+apm install linter-pylint
+apm install jsonlint
+apm install regex-railroad-diagram
+apm install sort-lines
